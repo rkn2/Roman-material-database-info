a2385d65c94b3230fbeac72dbecb8e0e5bf2aa94
rkn2 <rkn2@princeton.edu>
2017-11-13 13:38:43 -0500

    added ben's scripts for delimiter to csv

---
+++ data_cleaning/parseRomanTreestoCSV.py
@@ -0,0 +1,63 @@
+# List of wanted_fields we want
+# Imports
+import csv
+
+# The fields we want
+wanted_fields = ['Common Name(s)', 'Scientific Name', 'Distribution',
+                 'Tree Size', 'Average Dried Weight', 'Janka Hardness',
+                 'Modulus of Rupture', 'Elastic Modulus',
+                 'Crushing Strength', 'Shrinkage', 'Rot Resistance',
+                 'Workability', 'Common Uses'
+                ]
+# List of dictionaries with wanted_fields
+output_list = []
+
+"""
+This section of the script pulls in data from the parsed file and parses it based
+on the delimiters in roman_tree.py. It assumes that everything in the scrape
+worked correctly, so some html may slip through.  The final output is a Python
+dict for each line that then is appended to a list.
+
+By shifting indents these could easily be functionalized for repeated use.
+"""
+# Open file using 'with' so that it closes cleanly
+with open('trees.txt') as f:
+    for line in f:
+        # Define rolling variables for string cuts and an aggregated dict
+        start_cut = 0
+        end_cut = 0
+        aggregated_dict = {}
+        for i in range(len(wanted_fields)):
+            # Handle the first instance on a line
+            if start_cut == 0:
+                start_cut = line.find(":::")
+                start_cut += 3
+                end_cut = line.find(";;;")
+
+                aggregated_dict[wanted_fields[i]] = line[start_cut:end_cut]
+            # Handle all subsequent instances using previous start_ and end_cuts
+            else:
+                start_cut = line.find(":::", end_cut)
+                end_cut += 1
+                start_cut += 3
+                end_cut = line.find(";;;", end_cut)
+
+                aggregated_dict[wanted_fields[i]] = line[start_cut:end_cut]
+        # Append the dict
+        output_list.append(aggregated_dict)
+
+"""
+This section uses the csv module to write the dict to a standard CSV file that
+defaults to settings Excel likes to see.
+"""
+# Open file
+with open('output.csv', 'w') as csvfile:
+    # Invoke the csv.Dictwriter as 'writer'
+    writer = csv.DictWriter(csvfile, fieldnames=wanted_fields)
+
+    # Write the header line
+    writer.writeheader()
+
+    # Loop through the list of dictionaries and write each to a line
+    for dictionary in output_list:
+        writer.writerow(dictionary)
