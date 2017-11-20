a842d417579dd55136e937a51a4da6537e385092
rkn2 <rkn2@princeton.edu>
2017-11-20 13:17:02 -0500

    script to clean the csv file for trees

--- data_cleaning/clean_trees.py
+++ data_cleaning/clean_trees.py
@@ -1,5 +1,74 @@
 import csv
 
-with open ('output.csv') as oldfile, open()
+first = True;
+
+def getLoHiUnit(string):
+    a = string.find('(') + 1
+    b = string.find(')')
+    sub = string[a:b]
+    c = sub.find('-')
+    d = sub[c:].find(' ') + c
+    lo = sub[:c].strip(' ').strip('-')
+    hi = sub[c:d].strip(' ').strip('-')
+    unit = sub[d:].strip(' ').strip('-')
+    return [lo,hi,unit]
+
+def getValUnit(string):
+    a = string.find('(') + 1
+    b = string.find(')')
+    sub = string[a:b]
+    c = sub.find(' ')
+    d = sub[c:].find(' ') + c
+    val = sub[:c].strip(' ').strip('-')
+    unit = sub[d:].strip(' ').strip('-')
+    return [val,unit]
+
+def rowToCSV(row):
+    newRow = []
+    for item in row:
+        newRow.append('"%s"'%str(item))
+    return newRow
+
+newRows = [];
+with open ('output.csv') as fid:    
+    reader = csv.reader(fid,delimiter=',',quotechar='"')
+    for row in reader:
+        if first and len(row) > 0:
+            headers = row;
+            first = False;
+            continue
+        try:
+            # parse sizes (height, diameter)
+            sizes = row[3].split(',')
+            tall = getLoHiUnit(sizes[0])
+            diam = getLoHiUnit(sizes[1])
+            sizes = []
+            sizes.extend(tall)
+            sizes.extend(diam)
+            del row[3]
+            row.insert(3,sizes)
+            # parse miscellaneous other properties
+            for i in range(4,9):
+                vals = getValUnit(row[i])
+                del row[i]
+                row.insert(i,vals)
+            # parse shinkage
+            shrinkages = []
+            for s in row[9].split(','):
+                shrinkages.extend(getValUnit(s.strip(' ')))
+            del row[9]
+            row.insert(9,shrinkages)
+            # expand list
+            cleaned = []
+            for item in row:
+                if type(item) == type([]):
+                    cleaned.extend(item)
+                else:
+                    cleaned.append(item)
+            newRows.append(rowToCSV(cleaned))
+        except:
+            pass
     
-    
+with open('output_cleaned.csv','w') as fid:    
+    for row in newRows:
+        fid.write(','.join(row)+'\n')
